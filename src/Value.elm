3de0eed23acd586b13bc68f2fe501a5de2583160
zgeorg03 <zacharias33@gmail.com>
2016-07-17 00:18:24 +0300

    Date type added

--- src/Value.elm
+++ src/Value.elm
@@ -1,13 +1,68 @@
-module Value exposing (tests)
+module Value exposing (tests, Value)
 
 {-| This module wraps all the possible types a table cell can have
 
 
-@docs tests
+@docs tests, Value
 
 -}
 
 import ElmTest exposing (..)
+import Html exposing (..)
+import Date exposing (..)
+
+
+{-| This type holds the possible types for a table cell
+-}
+type Value
+    = I Int
+    | F Float
+    | B Bool
+    | S String
+    | D Int
+
+
+toString : Value -> String
+toString value =
+    case value of
+        I int ->
+            Basics.toString int
+
+        F float ->
+            Basics.toString float
+
+        B bool ->
+            Basics.toString bool
+
+        S string ->
+            string
+
+        D timestamp ->
+            let
+                date =
+                    getDateFromInt timestamp
+
+                day =
+                    Date.day date |> Basics.toString
+
+                month =
+                    getMonth date
+
+                year =
+                    Date.year date |> Basics.toString
+
+                hours =
+                    Date.hour date |> Basics.toString
+
+                mins =
+                    Date.minute date |> Basics.toString
+            in
+                day ++ " " ++ month ++ " " ++ year ++ ", " ++ hours ++ ":" ++ mins
+
+
+getDateFromInt : Int -> Date
+getDateFromInt timestamp =
+    (1000 * timestamp) |> toFloat |> Date.fromTime
 
 
 {-| The function with all the necessary tests
@@ -17,3 +72,62 @@ tests =
     suite "Value package Test suite"
         [ test "Addition" <| assertEqual (3 + 5) 8
         ]
+
+
+{-| This is just to visually check this module
+-}
+main : Html a
+main =
+    div []
+        [ div [] [ text (toString (I 2)) ]
+        , div [] [ text (toString (F 2.3)) ]
+        , div [] [ text (toString (D 1468688316)) ]
+        , div [] [ text (toString (B False)) ]
+        , div [] [ text (toString (S "Test")) ]
+        ]
+
+
+{-| Helper method to convert month type into its string representation
+-}
+getMonth : Date -> String
+getMonth date =
+    let
+        m =
+            Date.month date
+    in
+        case m of
+            Jan ->
+                "January"
+
+            Feb ->
+                "February"
+
+            Mar ->
+                "March"
+
+            Apr ->
+                "April"
+
+            May ->
+                "May"
+
+            Jun ->
+                "June"
+
+            Jul ->
+                "July"
+
+            Aug ->
+                "August"
+
+            Sep ->
+                "September"
+
+            Oct ->
+                "October"
+
+            Nov ->
+                "November"
+
+            Dec ->
+                "December"
